2a066695d8fe44320b4172b316e0d16f4fe838f7
Frederico Ferracini Duarte <frederico@ferracini.net.br>
2022-06-05 11:22:34 -0300

    New function create

--- index.js
+++ index.js
@@ -3,13 +3,20 @@
  * @since 2022-06-01 12:24:56
  */
 
-import { default as commonRoute } from './commonRoute.js'
-import { default as commonController } from './commonController.js'
+import { default as commonRoute } from './lib/commonRoute.js'
+import { default as commonController } from './lib/commonController.js'
 
 export const createCommonRoute = commonRoute
 export const createCommonController = commonController
+export const create = (router, route, model) => {
+	const commonController = new createCommonController(model)
+	createCommonRoute(router, route, commonController)
+
+	return commonController
+}
 
 export default {
+	create,
 	createCommonRoute,
 	createCommonController,
 }
